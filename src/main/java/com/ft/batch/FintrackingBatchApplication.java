fe6abd927aed0702de4bf12d3dfba72202789765
jae9380 <ljy5314@gmail.com>
2026-04-16 17:48:52 +0900

    Fintracking Batch Server

---
+++ src/main/java/com/ft/batch/FintrackingBatchApplication.java
@@ -0,0 +1,13 @@
+package com.ft.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackingBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintrackingBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ft/batch/FintrackingBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ft.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackingBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
